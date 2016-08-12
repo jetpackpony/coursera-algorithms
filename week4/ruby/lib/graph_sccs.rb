c4c1c04c05744e7024a3bcc4e5afd078b2b05dea
jetpackpony <jetpackpony@gmail.com>
2016-08-13 01:14:31 +0300

    Removed a bunch of instance variables

--- week4/ruby/lib/graph_sccs.rb
+++ week4/ruby/lib/graph_sccs.rb
@@ -2,9 +2,7 @@ module GraphSearch
   module SCCs
 
     def compute_sccs
-      @sccs = []
-      count_finishing_times
-      traverse_sccs
+      @sccs = collect_sccs calculate_finishing_times
     end
 
     def get_sccs
@@ -13,67 +11,70 @@ module GraphSearch
 
     private
 
-    def count_finishing_times
+    def ten_percent
+      (self.count / 10.0).ceil
+    end
+
+    def calculate_finishing_times
       log "  - counting finishing times"
       reverse
 
-      @finishing_times = []
-      @explored = Set.new
-      @ten_percent = (self.count / 10.0).ceil
-      self.each_index do |vert|
-        next if @explored.include? vert
-        dfs vert
-      end
-      reverse
-    end
+      finishing_times = []
+      explored = Set.new
 
-    def dfs(vert)
-      stack = [vert]
-      while stack.length > 0 do
-        v = stack.pop
-        if !@explored.include? v
-          if @explored.size % @ten_percent == 0
-            log "    - #{@explored.size / @ten_percent}0% done "
+      self.each_index do |vert|
+        next if explored.include? vert
+        stack = [vert]
+        while stack.length > 0 do
+          v = stack.pop
+          if !explored.include? v
+            if explored.size % ten_percent == 0
+              log "    - #{explored.size / ten_percent}0% done "
+            end
+            explored.add v
+            stack.push v
+            self[v].each do |edge|
+              next if explored.include? edge
+              stack.push edge
+            end
+          else
+            finishing_times.push v
           end
-          @explored.add v
-          stack.push v
-          self[v].each do |edge|
-            next if @explored.include? edge
-            stack.push edge
-          end
-        else
-          @finishing_times.push v
         end
       end
+
+      reverse
+      finishing_times
     end
 
-    def traverse_sccs
+    def collect_sccs(finishing_times)
       log "  - collecting sccs"
-      @explored = Set.new
-      @finishing_times.reverse.each do |vert|
-        next if @explored.include? vert
-        @current_scc = []
-        dfs_collect vert
-        @sccs.push @current_scc
+      explored = Set.new
+      sccs = []
+      finishing_times.reverse.each do |vert|
+        next if explored.include? vert
+        current_scc = []
+        explored.add vert
+        stack = [vert]
+        while stack.length > 0 do
+          if explored.size % ten_percent == 0
+            log "    - #{explored.size / ten_percent}0% done "
+          end
+
+          v = stack.pop
+          current_scc.push v
+          self[v].each do |edge|
+            next if explored.include? edge
+            explored.add edge
+            stack.push edge
+          end
+        end
+        sccs.push current_scc
       end
+      sccs
     end
 
     def dfs_collect(vert)
-      @explored.add vert
-      stack = [vert]
-      while stack.length > 0 do
-        if @explored.size % @ten_percent == 0
-          log "    - #{@explored.size / @ten_percent}0% done "
-        end
-
-        v = stack.pop
-        @current_scc.push v
-        self[v].each do |edge|
-          next if @explored.include? edge
-          @explored.add edge
-          stack.push edge
-        end
-      end
     end
   end
 end
